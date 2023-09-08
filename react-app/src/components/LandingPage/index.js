41648dab22177edf7005e9e73931c20a612a9338
Joshua <joshua.hulford@gmail.com>
2023-09-08 15:27:59 -0400

    create and connect landing page index.js and css

---
+++ react-app/src/components/LandingPage/index.js
@@ -0,0 +1,24 @@
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useHistory, NavLink } from "react-router-dom/cjs/react-router-dom.min";
+import "./LandingPage.css";
+
+const LandingPage = () => {
+  //Initialize things
+  const dispatch = useDispatch();
+  const history = useHistory();
+
+  //useSelectors
+
+  //State
+
+  //Dispatch
+
+  return (
+    <div>
+      <h1>Hello from landing page</h1>
+    </div>
+  );
+};
+
+export default LandingPage;
--- react-app/src/components/Navigation/index.js
+++ react-app/src/components/Navigation/index.js
@@ -1,24 +1,36 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
-import { useSelector } from 'react-redux';
-import ProfileButton from './ProfileButton';
-import './Navigation.css';
+import React from "react";
+import { NavLink } from "react-router-dom";
+import { useSelector } from "react-redux";
+import ProfileButton from "./ProfileButton";
+import "./Navigation.css";
 
-function Navigation({ isLoaded }){
-	const sessionUser = useSelector(state => state.session.user);
+function Navigation({ isLoaded }) {
+  const sessionUser = useSelector((state) => state.session.user);
 
-	return (
-		<ul>
-			<li>
-				<NavLink exact to="/">Home</NavLink>
-			</li>
-			{isLoaded && (
-				<li>
-					<ProfileButton user={sessionUser} />
-				</li>
-			)}
-		</ul>
-	);
+  return (
+    <ul className="navigation-ul">
+      <li>
+        <NavLink exact to="/">
+          Home
+        </NavLink>
+      </li>
+      <li>
+        <NavLink exact to="/">
+          Places
+        </NavLink>
+      </li>
+      <li>
+        <NavLink exact to="/">
+          Preferences
+        </NavLink>
+      </li>
+      {isLoaded && (
+        <li>
+          <ProfileButton user={sessionUser} />
+        </li>
+      )}
+    </ul>
+  );
 }
 
 export default Navigation;
