07be0629057df8291fe3e3a8b19026ef9a0b5e10
Joshua <joshua.hulford@gmail.com>
2023-09-11 14:31:17 -0400

    add logo home link to nav bar

--- react-app/src/components/Navigation/index.js
+++ react-app/src/components/Navigation/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 import { useSelector } from "react-redux";
 import ProfileButton from "./ProfileButton";
+import logo from "../Navigation/SensoryScore.png";
 import "./Navigation.css";
 
 function Navigation({ isLoaded }) {
@@ -11,7 +12,11 @@ function Navigation({ isLoaded }) {
     <ul className="navigation-ul">
       <li>
         <NavLink exact to="/">
-          Home
+          <img
+            className="navbar-logo"
+            src={logo}
+            alt="Sensory Score Logo and Link to Home Page"
+          ></img>
         </NavLink>
       </li>
       <li>
